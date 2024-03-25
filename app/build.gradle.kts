04a4d8533b006aa2196d3e063884aa9cfbefd378
dhaliwal918 <dhaliwal090108@gmail.com>
2024-03-25 22:38:13 +0530

    new

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +44,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //this
+//    implementation(de.hdodenhof:circleimageview:3.1.0)
+    implementation 'de.hdodenhof:circleimageview:3.1.0'
+
 }
